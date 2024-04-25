40971d175f7ee9ea58d58a509759d30eb0ee4ec8
letriit97 <75650934+letriit97@users.noreply.github.com>
2024-04-25 12:24:00 +0700

    Hoàn thành demo page 2

---
+++ js/dashboard2.js
@@ -0,0 +1,55 @@
+//  Khi bấm nút next | previous để chuyển trang
+const pageTurnBtn = document.querySelectorAll(".next-previout-btn");
+pageTurnBtn.forEach((element, index) => {
+    element.onclick = () => {
+        const pageTurnId = element.getAttribute('data-page');
+        console.log("pageTurnBtn.forEach - pageTurnId:", pageTurnId)
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        }
+        else {
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            }, 500)
+        }
+    }
+})
+
+const pages = document.querySelectorAll('.book-page.page-right'); // danh sách pages
+let totalPages = pages.length;
+let pageNumber = 0;
+const contactMessage = document.querySelector('.back-profile'); // nút gửi liên hệ
+
+function revergePageIndex() {
+    pageNumber--;
+    if (pageNumber < 0)
+        pageNumber = totalPages - 1;
+}
+
+// Bấm nút thông tin cá nhân
+contactMessage.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            revergePageIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                revergePageIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+        }, (index + 1) * 200 + 100);
+    })
+}
+
+
+
+
+
+
+
